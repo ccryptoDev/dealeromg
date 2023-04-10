5ca83d5cf7231d2a56bb689955fa846754725d5b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-09 23:31:15 -0400

    modified routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -28,270 +28,276 @@ import DvSystemAlerts from "../pages/AdminPanel/DvSystemAlerts"
 
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/target-market/audience-builder"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <AudienceBuilder />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/target-market/customer-builder"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <CustomerBuilder />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/target-market/user-builds"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <UserBuilds />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/target-market/dealer-builds"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <DealerBuilds />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/target-market/global-builds"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <GlobalBuilds />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/profile"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={0} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/inventory-urls"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={1} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/customize"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={2} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/connect"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={3} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/hierarchy"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={4} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/alerts"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={5} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/business-settings/alert-control"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <BusinessSettings activeTab={6} />
-              </WithPermissionRouter>
-            }
-          />
-          <Route exact path="/admin-panel/new-user" element={<NewUser />} />
-          <Route
-            exact
-            path="/admin-panel/user-management"
-            element={<UserManagement />}
-          />
-          <Route
-            exact
-            path="/admin-panel/feed-providers"
-            element={<FeedProvider />}
-          />
-          <Route
-            exact
-            path="/admin-panel/user-bulk-upload"
-            element={<AdminPanel />}
-          />
-          <Route exact path="/admin-panel/customize" element={<AdminPanel />} />
-          <Route
-            exact
-            path="/admin-panel/dealer-list"
-            element={<AdminPanel />}
-          />
-          <Route
-            exact
-            path="/admin-panel/provider-management"
-            element={<ProviderManagement />}
-          />
-          <Route
-            exact
-            path="/admin-panel/feed-system-alerts"
-            element={<SystemAlerts />}
-          />
-          <Route
-            exact
-            path="/admin-panel/dv-system-alerts"
-            element={<DvSystemAlerts />}
-          />
-          <Route
-            exact
-            path="/campaigns/inventory-builder"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <InventoryBuilder />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/campaigns/sold-table"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <Sold />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/campaigns/irregular"
-            element={
-              <WithPermissionRouter
-                rols={["super-admin", "admin", "Management", "Staff"]}
-                grantByRols={true}
-                requireDealer={true}
-              >
-                <Irregular />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/inventory/outbound/:platform_id"
-            element={<FilesList />}
-          />
-          <Route exact path="/performance" element={<Performance />} />
-          <Route path="/login" element={<SimpleLoginPage />} />
-          <Route path="/" element={<EntryPage />} />
-          <Route
-            exact
-            path="/forgot-password"
-            element={
-              <WithPermissionRouter loggedNeeded={false}>
-                <ForgotPassword />
-              </WithPermissionRouter>
-            }
-          />
-          <Route
-            exact
-            path="/recover-password"
-            element={
-              <WithPermissionRouter loggedNeeded={false}>
-                <RecoveryPassword />
-              </WithPermissionRouter>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <div className="bg-[#002E5D] h-screen">
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              exact
+              path="/target-market/audience-builder"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <AudienceBuilder />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/target-market/customer-builder"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <CustomerBuilder />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/target-market/user-builds"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <UserBuilds />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/target-market/dealer-builds"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <DealerBuilds />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/target-market/global-builds"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <GlobalBuilds />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/profile"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={0} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/inventory-urls"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={1} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/customize"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={2} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/connect"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={3} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/hierarchy"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={4} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/alerts"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={5} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/business-settings/alert-control"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <BusinessSettings activeTab={6} />
+                </WithPermissionRouter>
+              }
+            />
+            <Route exact path="/admin-panel/new-user" element={<NewUser />} />
+            <Route
+              exact
+              path="/admin-panel/user-management"
+              element={<UserManagement />}
+            />
+            <Route
+              exact
+              path="/admin-panel/feed-providers"
+              element={<FeedProvider />}
+            />
+            <Route
+              exact
+              path="/admin-panel/user-bulk-upload"
+              element={<AdminPanel />}
+            />
+            <Route
+              exact
+              path="/admin-panel/customize"
+              element={<AdminPanel />}
+            />
+            <Route
+              exact
+              path="/admin-panel/dealer-list"
+              element={<AdminPanel />}
+            />
+            <Route
+              exact
+              path="/admin-panel/provider-management"
+              element={<ProviderManagement />}
+            />
+            <Route
+              exact
+              path="/admin-panel/feed-system-alerts"
+              element={<SystemAlerts />}
+            />
+            <Route
+              exact
+              path="/admin-panel/dv-system-alerts"
+              element={<DvSystemAlerts />}
+            />
+            <Route
+              exact
+              path="/campaigns/inventory-builder"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <InventoryBuilder />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/campaigns/sold-table"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <Sold />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/campaigns/irregular"
+              element={
+                <WithPermissionRouter
+                  rols={["super-admin", "admin", "Management", "Staff"]}
+                  grantByRols={true}
+                  requireDealer={true}
+                >
+                  <Irregular />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/inventory/outbound/:platform_id"
+              element={<FilesList />}
+            />
+            <Route exact path="/performance" element={<Performance />} />
+            <Route path="/login" element={<SimpleLoginPage />} />
+            <Route path="/" element={<EntryPage />} />
+            <Route
+              exact
+              path="/forgot-password"
+              element={
+                <WithPermissionRouter loggedNeeded={false}>
+                  <ForgotPassword />
+                </WithPermissionRouter>
+              }
+            />
+            <Route
+              exact
+              path="/recover-password"
+              element={
+                <WithPermissionRouter loggedNeeded={false}>
+                  <RecoveryPassword />
+                </WithPermissionRouter>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
   )
 }
 
