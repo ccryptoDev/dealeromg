34f7fedd7b2fa3f4b4e2ee4a1c4d88b46de48379
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-04-11 20:12:34 -0400

    added recoil

--- src/pages/EntryPage.jsx
+++ src/pages/EntryPage.jsx
@@ -1,26 +1,60 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-import useAuth from "../Hooks/useAuth";
+import React, { useEffect } from "react"
+import { useRecoilState } from "recoil"
 // Components
-import LayoutEntryPage from "../containers/EntryPage/LayoutEntryPage";
+import LayoutEntryPage from "../containers/EntryPage/LayoutEntryPage"
+import {
+  filtersValuesStateCB,
+  recordCountValuesStateCB,
+  FinalWhereClsCBSale,
+  FinalWhereClsCBService,
+  recordCountNumberCB,
+} from "../atoms/CustomerBuilderAtom"
+import {
+  filtersValuesState,
+  recordCountValuesState,
+  FinalWhereClsAM,
+  recordCountNumber,
+} from "../atoms/audienceCatBuilderAtom"
+
+import {
+  defaultFilterValuesAB,
+  defaultFilterValuesCB,
+  defaultRecordCountValuesAB,
+  defaultRecordCountValuesCB,
+} from "../util/defaultValuesFilterBuilders"
 
 function EntryPage() {
-  const authPermRols = useAuth([""], false);
-  const history = useNavigate();
+  const setFiltersValuesCB = useRecoilState(filtersValuesStateCB)[1]
+  const setRecordCountValuesCB = useRecoilState(recordCountValuesStateCB)[1]
+  const setFiltersValues = useRecoilState(filtersValuesState)[1]
+  const setRecordCountValues = useRecoilState(recordCountValuesState)[1]
+  const setSQLAudience = useRecoilState(FinalWhereClsAM)[1]
+  const setSQLSale = useRecoilState(FinalWhereClsCBSale)[1]
+  const setSQLService = useRecoilState(FinalWhereClsCBService)[1]
+  const setRecordCountNumber = useRecoilState(recordCountNumber)[1]
+  const setRecordCountNumberCB = useRecoilState(recordCountNumberCB)[1]
+
+  const resetFiltersAudience = () => {
+    setFiltersValues(defaultFilterValuesAB)
+    setRecordCountValues(defaultRecordCountValuesAB)
+    setFiltersValuesCB(defaultFilterValuesCB)
+    setRecordCountValuesCB(defaultRecordCountValuesCB)
+    setSQLAudience({ sql: "" })
+    setSQLSale({ sql: " AND 1=0" })
+    setSQLService({ sql: " AND 1=0" })
+    setRecordCountNumber({ value: null })
+    setRecordCountNumberCB({ value: null })
+  }
 
   useEffect(() => {
-    if (!authPermRols[0]) {
-      history("/login");
-      return null;
-    }
-  }, []);
+    resetFiltersAudience(true)
+  }, [])
 
   return (
     <div className="flex flex-col">
       <LayoutEntryPage />
     </div>
-  );
+  )
 }
 
-export default EntryPage;
+export default EntryPage
