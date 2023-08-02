e01a8800c46fbdb20b21ec64109fb33cd4ddb073
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-08-02 12:42:40 -0400

    wrong spell

--- src/components/CustomerBuilder/PrevSerROCode.jsx
+++ src/components/CustomerBuilder/PrevSerROCode.jsx
@@ -4,7 +4,7 @@ import SwitchFilter from "../Fields/SwitchFilter"
 const PrevSerROCode = () => {
   return (
     <SwitchFilter
-      next={"No Service in a Date Range"}
+      next={"No Services in a Date Range"}
       prev={"Never Purchased a Vehicle"}
       custBuild={true}
     />
