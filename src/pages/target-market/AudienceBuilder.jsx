8c4bcc6a5ca030c883ecc770ee8de843b1fda71b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-16 08:20:26 -0400

    DOG-183

--- src/pages/target-market/AudienceBuilder.jsx
+++ src/pages/target-market/AudienceBuilder.jsx
@@ -1,25 +1,11 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-import useAuth from "../../Hooks/useAuth";
-import LayoutAudienceBuilder from "../../containers/AudienceCatBuilder/LayoutAudienceBuilder";
+import LayoutAudienceBuilder from "../../containers/AudienceCatBuilder/LayoutAudienceBuilder"
 
 function AudienceBuilder() {
-  const authPermRols = useAuth([""], false);
-  const history = useNavigate();
-
-  useEffect(() => {
-    if (!authPermRols[0]) {
-      history("/login");
-      return null;
-    }
-  }, []);
-
   return (
     <div>
       <LayoutAudienceBuilder />
     </div>
-  );
+  )
 }
 
-export default AudienceBuilder;
+export default AudienceBuilder
