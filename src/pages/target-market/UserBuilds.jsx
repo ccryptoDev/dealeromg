06272f518d5c4a66510ce695b66b5bbd05074990
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-09-16 08:33:45 -0400

    DOG-205

--- src/pages/target-market/UserBuilds.jsx
+++ src/pages/target-market/UserBuilds.jsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 import axios from "axios"
 
-import useAuth from "../../Hooks/useAuth"
 import LayoutPresetBuilds from "../../containers/PresetBuilds/LayoutPresetBuilds"
 
 function UserBuilds() {
-  const authPermRols = useAuth([""], false)
-  const history = useNavigate()
   const [data, setData] = useState([])
   const user = JSON.parse(localStorage.getItem("permissionsDealerOMG"))
   const getPresets = () => {
@@ -26,10 +22,6 @@ function UserBuilds() {
   }
 
   useEffect(() => {
-    if (!authPermRols[0]) {
-      history("/login")
-      return null
-    }
     getPresets()
   }, [])
 
